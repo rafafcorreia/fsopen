bdc0c28261695db0fb86215a548719fa956524f2
rafafcorreia <rafa.f.correia@gmail.com>
2026-09-09 17:52:29 -0300

    Done exercise 1.2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -5,15 +5,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </>
   );
 };
@@ -23,6 +17,14 @@ const Total = (props) => {
     <p>
       Number of exercises{' '}
       {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
     </p>
   );
 };
