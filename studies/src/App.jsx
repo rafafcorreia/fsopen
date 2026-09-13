fbd13fbeac7681f97c4c9aaf7e1b58d97ecc7434
rafafcorreia <rafa.f.correia@gmail.com>
2026-09-13 14:22:20 -0300

    studies project: done learning the part 1

--- studies/src/App.jsx
+++ studies/src/App.jsx
@@ -1,20 +1,20 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
+import { useState } from 'react';
+
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const friends = ['Peter', 'Maya'];
+  const [value, setValue] = useState(10);
+
+  const setToValue = (newValue) => setValue(newValue);
 
   return (
     <div>
-      <p>{friends}</p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
