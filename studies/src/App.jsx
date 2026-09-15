25cfc0e6eef443d5a2f16d1a6bcc50850e824642
rafafcorreia <rafa.f.correia@gmail.com>
2026-09-15 17:25:34 -0300

    studies: done studying 2a

--- studies/src/App.jsx
+++ studies/src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react';
+import Note from './components/Note';
 
-const Display = (props) => <div>{props.value}</div>;
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
-const App = () => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = (newValue) => setValue(newValue);
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text="thousand" />
-      <Button onClick={() => setToValue(0)} text="reset" />
-      <Button onClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} content={note.content} />
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ studies/src/components/Note.jsx
@@ -0,0 +1,3 @@
+const Note = ({ content }) => <li>{content}</li>;
+
+export default Note;
